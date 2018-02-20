b2307080b3b0ad3ab0e0d702999e58aabed94b1a
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    picasso: Build LiveDisplay HAL
    Change-Id: I4bfac85c64599ced45983a86d98cfd810a97db99

--- device.mk
+++ device.mk
@@ -262,6 +262,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
