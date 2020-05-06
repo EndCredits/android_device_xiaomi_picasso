a872d8d40d1ee84657e5dd20b665eac96ef3d9cd
razorloves <razorloves@gmail.com>
2020-05-05 21:07:30 -0500

    picasso: Build Snap camera app
    Change-Id: I1a0ed1e9518f6e20a264632243a30847157acc9f

--- device.mk
+++ device.mk
@@ -166,6 +166,7 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
+    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
