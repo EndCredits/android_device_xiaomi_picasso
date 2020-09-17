973a720eb87f0e58f2cc04ccc149bf3a41c748a2
Fei Zheng <zfei@codeaurora.org>
2020-09-17 14:21:39 +0800

    picasso: Disable support for radio FM (frequency modulation).
     * QCOM has started disabling support for FM for all
       non-automotive platforms.
    
    Change-Id: I6cbed0e060141a27becd22d673bbc076617e98c7

--- device.mk
+++ device.mk
@@ -166,6 +166,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
 # Bluetooth
+BOARD_HAVE_QCOM_FM := false
+
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
