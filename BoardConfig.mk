ec18e856d6b0987d713e5be3a75db9c2c3c5c54a
TH779 <tongsj2333@gmail.com>
2021-02-24 13:31:04 +0800

    picasso: Enable DT2W feature
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: Ie036a52d2475785c79751d474a907e3befa63c9c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,9 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
 
