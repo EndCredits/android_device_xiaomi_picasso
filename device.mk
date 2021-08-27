c25d1bd975a9dcd8da6ac86fddebc067ece05a08
TH779 <i@779.moe>
2021-08-27 16:58:11 +0800

    picasso: Build fastbootd
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Ib6384c68f326ce375c6329ff748bba5e660f8eec

--- device.mk
+++ device.mk
@@ -201,6 +201,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_picasso
