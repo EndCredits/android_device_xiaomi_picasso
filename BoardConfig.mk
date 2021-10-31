f5aa475f574ff6b757574fd7a98f1727d58decac
TH779 <i@779.moe>
2021-10-31 11:32:08 +0800

    [DNM] picasso: Set SELinux to permissive
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Iaecc1a610b247845ed9c094535b8f99a3c7443a5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
