0365adc167e614b8562bf9e5af01611702c6cae3
Alexander Koskovich <zvnexus@outlook.com>
2021-11-04 13:31:47 +0800

    picasso: Move compatibility matrix to QCOM common.
    Change-Id: Iafb6a3f7eb4130fdc5264abb04142fe5580a1fb6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := device/qcom/common/compatibility_matrix.xml
 
 # Input
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 102
