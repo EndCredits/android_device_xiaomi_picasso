7c0ad4e19e5558268fee2f6434c564d656d10325
baolong24 <1667375308@qq.com>
2021-11-23 11:59:31 +0800

    [DNM] picasso: Disable ifaa

--- device.mk
+++ device.mk
@@ -271,8 +271,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+#PRODUCT_BOOT_JARS += \
+#    org.ifaa.android.manager
 
 # IPACM
 PRODUCT_PACKAGES += \
