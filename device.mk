06286cc893ab9d0a96c32ac8f88aac1c5ae242f2
Zinadin Zidan <zidan.roking@gmail.com>
2021-12-09 04:57:19 +0200

    picasso: Guard qti thermal 2.0 service hals * Its crashes on picasso  but fine on other devices
     * Crash:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.

--- device.mk
+++ device.mk
@@ -453,8 +453,10 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
+endif
 
 # Touchscreen
 PRODUCT_PACKAGES += \
