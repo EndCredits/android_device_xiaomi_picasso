57b4b6bf70e1213f858eac965944afff4a71987c
dinhsan2000 <dinhsan200@gmail.com>
2021-12-23 03:45:03 +0700

    Picasso: Update Removepackage
    Signed-off-by: dinhsan2000 <dinhsan200@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup Drive FM2 Maps MyVerizonServices OBDM_Permissions OemDmTrigger PrebuiltGmail Showcase SprintDM SprintHM YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio Photos Calculator Calender NfcNci TipsPrebuilt GoogleTTS Music MicropaperPrebuilt NgaResources RecorderPrebuilt SafetyHubPrebuilt WallpapersBReel2020 SoundAmplifierPrebuilt AppDirectedSMSService ConnMO DCMO USCCDM arcore talkback DevicePolicyPrebuilt AndroidAutoStubPrebuilt Via PlayGames GooglePlayServicesforAR ARCore FilesbyGoogle KeepNotes Keep PixelLiveWallpaper Sounds PixelBuds
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Drive FM2 MyVerizonServices OBDM_Permissions OemDmTrigger PrebuiltGmail Showcase SprintDM SprintHM YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio NfcNci TipsPrebuilt MicropaperPrebuilt NgaResources RecorderPrebuilt SafetyHubPrebuilt WallpapersBReel2020 SoundAmplifierPrebuilt AppDirectedSMSService ConnMO DCMO USCCDM arcore talkback DevicePolicyPrebuilt AndroidAutoStubPrebuilt Via PlayGames GooglePlayServicesforAR ARCore KeepNotes Keep PixelLiveWallpaper PixelBuds
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
