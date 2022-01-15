e1ec008a187f6d458ee84238d040a96db5c0b711
dreamfan92 <medicspam6@gmail.com>
2022-01-15 20:09:35 +0700

    picasso: Remove old DRM dependencies

--- device.mk
+++ device.mk
@@ -214,16 +214,6 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0 \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.1 \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2 \
-    android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3 \
-    android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
