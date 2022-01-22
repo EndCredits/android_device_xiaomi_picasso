a365fa61df0a7730cb99ed469661fc3ac3d0b89d
dreamfan92 <medicspam6@gmail.com>
2022-01-22 10:08:37 +0700

    picasso: Refactor parts from previous tree & add CameraGo
    This also add Chinese & Vietnamese translation into XiaomiParts
    
    Signed-off by: dreamfan92 <medicspam6@gmail.com>

--- device.mk
+++ device.mk
@@ -169,7 +169,7 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
-    Camera2 \
+    CameraGo \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- *               2017 The LineageOS Project
+ *               2017-2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,40 +21,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.content.SharedPreferences;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.thermal.ThermalUtils;
-
-import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
-
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
-    public static final String SHAREDD2TW = "sharadeD2TW";
-    private ITouchFeature mTouchFeature;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        try {
         DiracUtils.initialize(context);
-        } catch (Exception e) {
-            Log.d(TAG, "Dirac is not present in system");
-        }
         DozeUtils.checkDozeService(context);
-        ThermalUtils.startService(context);
-        
-        //Micro-Service to restore sata of dt2w on reboot
-        SharedPreferences prefs = context.getSharedPreferences(SHAREDD2TW, Context.MODE_PRIVATE);
-        try {
-            mTouchFeature = ITouchFeature.getService();
-            mTouchFeature.setTouchMode(14,prefs.getInt(SHAREDD2TW, 1));
-        } catch (Exception e) {
-            // Do nothing
-        }
     }
 }
