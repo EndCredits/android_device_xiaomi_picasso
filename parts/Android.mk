22dad79c5370b68d91d4d3c25af6ab6a1ef037aa
dreamfan92 <medicspam6@gmail.com>
2022-01-22 10:37:16 +0700

    picasso: Add missing Dirac enable string

--- parts/Android.mk
+++ parts/Android.mk
@@ -22,8 +22,7 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
-    androidx.preference_preference \
-    org.pixelexperience.settings.resources
+    androidx.preference_preference 
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
