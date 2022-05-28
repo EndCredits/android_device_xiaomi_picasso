2f9635838368d52fe1e220f9293df2bdf149479b
SebaUbuntu <barezzisebastiano@gmail.com>
2022-05-28 22:47:16 +0200

    picasso: Move to common Xiaomi fingerprint HIDL
    Change-Id: I6c29a831e985ee440c92a98aad8d33f25b9e5b2d
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- device.mk
+++ device.mk
@@ -225,7 +225,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_picasso
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
