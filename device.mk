7c03c113230795cae668dd921c05f6f3126cd04c
EndCredits <alicization.han@gmail.com>
2022-07-12 12:55:23 +0800

    picasso: Roll back USB HAL to version 1.2
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- device.mk
+++ device.mk
@@ -487,7 +487,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.2-service-qti
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
