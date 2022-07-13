764bd5322f9d463990ad9843ebf26d88e741b8e3
EndCredits <alicization.han@gmail.com>
2022-07-13 14:42:00 +0800

    picasso: Build USB HAL 1.3 and boot scripts from CodeLinaro
    * On Android 12.1, it seems that stock usb configuration scripts can't
      support our USB MTP Settings. Let's try CodeLinaro's version.
    
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- device.mk
+++ device.mk
@@ -283,8 +283,6 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sh \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     init.xiaomi.rc \
@@ -491,6 +489,13 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
