4c2ab71d9150bed573529c3523c49138c2dfc814
EndCredits <alicization.han@gmail.com>
2022-08-08 17:01:36 +0800

    picasso: Initial ArrowOS Bring up
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ $(call inherit-product, vendor/xiaomi/picasso-firmware/firmware.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-kscope
+    $(LOCAL_PATH)/overlay-arrow
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
