efae38ea8d777ddd2b80e831580e162496c26be6
masemoel <az.az.az.mjme@gmail.com>
2022-08-14 07:57:38 +0800

    picasso: Reimport parts
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- parts/src/org/lineageos/settings/doze/ProximitySensor.java
+++ parts/src/org/lineageos/settings/doze/ProximitySensor.java
@@ -22,6 +22,9 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.SystemClock;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.util.Log;
 
 import java.util.concurrent.ExecutorService;
@@ -33,37 +36,49 @@ public class ProximitySensor implements SensorEventListener {
     private static final boolean DEBUG = false;
     private static final String TAG = "ProximitySensor";
 
-    // Maximum time for the hand to cover the sensor: 1s
-    private static final int HANDWAVE_MAX_DELTA_NS = 1000 * 1000 * 1000;
-
-    // Minimum time until the device is considered to have been in the pocket: 2s
-    private static final int POCKET_MIN_DELTA_NS = 2000 * 1000 * 1000;
+    private static final int POCKET_DELTA_NS = 1000 * 1000 * 1000;
+    private static final int WAKELOCK_TIMEOUT_MS = 300;
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private Context mContext;
-    private ExecutorService mExecutorService;
+    private PowerManager mPowerManager;
+    private WakeLock mWakeLock;
 
     private boolean mSawNear = false;
     private long mInPocketTime = 0;
 
+    private boolean mHandwaveGestureEnabled;
+    private boolean mPocketGestureEnabled;
+
+    private final ExecutorService mExecutorService;
+
     public ProximitySensor(Context context) {
         mContext = context;
-        mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
+        mSensorManager = (SensorManager)
+                mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = DozeUtils.getSensor(mSensorManager, "xiaomi.sensor.proxmity.factory");
+        if (mSensor == null) {
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        }
+        mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
         mExecutorService = Executors.newSingleThreadExecutor();
-    }
-
-    private Future<?> submit(Runnable runnable) {
-        return mExecutorService.submit(runnable);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        boolean isRaiseToWake = DozeUtils.isRaiseToWakeEnabled(mContext);
         boolean isNear = event.values[0] < mSensor.getMaximumRange();
         if (mSawNear && !isNear) {
             if (shouldPulse(event.timestamp)) {
-                DozeUtils.launchDozePulse(mContext);
+                if (isRaiseToWake) {
+                    mWakeLock.acquire(WAKELOCK_TIMEOUT_MS);
+                    mPowerManager.wakeUp(SystemClock.uptimeMillis(),
+                            PowerManager.WAKE_REASON_GESTURE, TAG);
+                } else {
+                    DozeUtils.wakeOrLaunchDozePulse(mContext);
+                }
             }
         } else {
             mInPocketTime = event.timestamp;
@@ -71,16 +86,19 @@ public class ProximitySensor implements SensorEventListener {
         mSawNear = isNear;
     }
 
+    private Future<?> submit(Runnable runnable) {
+        return mExecutorService.submit(runnable);
+    }
+
     private boolean shouldPulse(long timestamp) {
         long delta = timestamp - mInPocketTime;
 
-        if (DozeUtils.isHandwaveGestureEnabled(mContext) &&
-                DozeUtils.isPocketGestureEnabled(mContext)) {
+        if (mHandwaveGestureEnabled && mPocketGestureEnabled) {
             return true;
-        } else if (DozeUtils.isHandwaveGestureEnabled(mContext)) {
-            return delta < HANDWAVE_MAX_DELTA_NS;
-        } else if (DozeUtils.isPocketGestureEnabled(mContext)) {
-            return delta >= POCKET_MIN_DELTA_NS;
+        } else if (mHandwaveGestureEnabled && !mPocketGestureEnabled) {
+            return delta < POCKET_DELTA_NS;
+        } else if (!mHandwaveGestureEnabled && mPocketGestureEnabled) {
+            return delta >= POCKET_DELTA_NS;
         }
         return false;
     }
@@ -90,18 +108,28 @@ public class ProximitySensor implements SensorEventListener {
         /* Empty */
     }
 
+    // Switching screen OFF - we enable the sensor
     protected void enable() {
         if (DEBUG) Log.d(TAG, "Enabling");
         submit(() -> {
-            mSensorManager.registerListener(this, mSensor,
-                    SensorManager.SENSOR_DELAY_NORMAL);
+            // We save user settings so at next screen ON call (enable())
+            // we don't need to read them again from the Settings provider
+            mHandwaveGestureEnabled = DozeUtils.isHandwaveGestureEnabled(mContext);
+            mPocketGestureEnabled = DozeUtils.isPocketGestureEnabled(mContext);
+            if (mHandwaveGestureEnabled || mPocketGestureEnabled) {
+                mSensorManager.registerListener(this, mSensor,
+                        SensorManager.SENSOR_DELAY_NORMAL);
+            }
         });
     }
 
+    // Switching screen ON - we disable the sensor
     protected void disable() {
         if (DEBUG) Log.d(TAG, "Disabling");
         submit(() -> {
-            mSensorManager.unregisterListener(this, mSensor);
+            if (mHandwaveGestureEnabled || mPocketGestureEnabled) {
+                mSensorManager.unregisterListener(this, mSensor);
+            }
         });
     }
 }
