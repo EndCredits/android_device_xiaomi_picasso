d8d58c5653933fd535c965981213c3d29d623f65
EndCredits <alicization.han@gmail.com>
2022-08-16 21:47:45 +0800

    picasso: Build sensor HAL 1.0
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- device.mk
+++ device.mk
@@ -455,6 +455,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-service \
     android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
