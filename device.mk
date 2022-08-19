d538f7299c657dba73444efb03e91ed3ffc4ffdc
mikairyuu <miva200267@gmail.com>
2022-08-20 00:42:00 +0300

    picasso: Uprev audio HAL to 7.1
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- device.mk
+++ device.mk
@@ -95,7 +95,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl \
