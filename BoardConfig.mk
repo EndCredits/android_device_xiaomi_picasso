7f4f9dc5c82899f05ee2c5f419dc1ed29746b846
EndCredits <alicization.han@gmail.com>
2022-08-21 10:00:25 +0800

    picasso: Build vendor framework compatibility matrix
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,11 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml \
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+
 DEVICE_MATRIX_FILE := device/qcom/common/compatibility_matrix.xml
 
 # Input
