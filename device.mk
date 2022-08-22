59b90692cc0cdd42420e47d2bf1d8dc85e3d013c
EndCredits <alicization.han@gmail.com>
2022-08-22 17:17:40 +0800

    picasso: Stop build render scripts
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- device.mk
+++ device.mk
@@ -358,10 +358,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
-# RenderScript
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-    
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
     av \
