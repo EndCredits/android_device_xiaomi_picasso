e0b558f2f81991ff88b878b45b746b82f5073d4a
EndCredits <alicization.han@gmail.com>
2022-09-10 19:19:14 +0800

    picasso: Build android.hardware.drm@1.0
    * Crash:
    08-18 13:11:23.238  2445  2445 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.servicetracker@1.2-service": library "vendor.qti.hardware.servicetracker@1.2.so" not found: needed by main executable
    08-18 13:11:24.223  2447  2447 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/wfdhdcphalservice": library "android.hardware.drm@1.0.so" not found: needed by main executable
    
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- device.mk
+++ device.mk
@@ -224,6 +224,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0 \
+    android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.4-service.clearkey
     
 # Fastbootd
