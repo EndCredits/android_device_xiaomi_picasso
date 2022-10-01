724ba553e894235019c1d5ea4f9814cb34ea7d03
EndCredits <alicization.han@gmail.com>
2022-10-01 08:16:46 +0800

    Revert "picasso: Import LineageOS FCM"
    This reverts commit de33281182ca5ac168265594049d9ef82add31b0.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,9 +99,6 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-	vendor/lineage/config/device_framework_matrix.xml
-
 # Input
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 102
 
