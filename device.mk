0d41408a5dfc651ac465011d3d7e2ede28d1c71b
johnmart19 <johnivan19999@gmail.com>
2022-10-02 19:27:39 +0300

    picasso: Audio: Build android.hardware.audio@7.1-util
    - Matching stock environment
    - Also provies compilation of:
    * android.hardware.audio@7.1
    * android.hardware.audio.common@7.0
    * android.hardware.audio.common@7.1-enums
    * android.hardware.audio.common@7.1-util
    
    Currently in system/lib we had only android.hardware.audio.common@2.0 & @5.0
    In system/lib64 we had only android.hardware.audio.common@2.0.so @4.0 @4.0-util @5.0 @5.0-util @6.0 @6.0-util and AIDL
    
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- device.mk
+++ device.mk
@@ -96,6 +96,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
+    android.hardware.audio@7.1-util \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl \
