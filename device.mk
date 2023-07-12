f856ce7463304f0ae05bccd611cd55d34513735e
EndCredits <endcredits@crepuscular-aosp.icu>
2023-07-12 09:31:44 +0800

    picasso: Clean up unecessary bluetooth blobs
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- device.mk
+++ device.mk
@@ -162,18 +162,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
-# Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio-impl \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
-
-PRODUCT_PACKAGES += \
-    libldacBT_bco \
-    libldacBT_dec
     
 # Camera
 PRODUCT_PACKAGES += \
